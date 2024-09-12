4bc6ea300c18ceb808f63bdba8d12bb863da98d3
Sathish-43 <20csa43@karpagamtech.ac.in>
2024-09-12 17:44:17 +0530

    hello

--- app.py
+++ app.py
@@ -147,5 +147,6 @@ def submit_quiz():
         return f"Your score is {score}/{total}. Results saved and emailed successfully!"
     return redirect(url_for('home'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
